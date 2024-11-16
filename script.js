76351ddfe185849c3d3a209f9abff420661a8a17
JamesCoalchi <jamie@jamescoalchi.space>
2024-11-16 22:07:02 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+let confirmationCount = 0;
+const maxConfirmations = 5;
+document.querySelector('.buy-button').addEventListener('click', function() {
+  confirmationCount++;
+  if (confirmationCount < maxConfirmations) {
+    const messages = [
+      "Вы уверены?",
+      "А точно уверены?",
+      "Вы правда хотите это сделать?",
+      "Почему вы это нажимаете?",
+      "..."
+      "Вы не передумали, точно? 😏"
+    ];
+    if (!confirm(messages[confirmationCount - 1])) {
+      return;
+    }
+  } else {
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+  }
+});
